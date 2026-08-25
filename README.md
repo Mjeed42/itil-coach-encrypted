1a68fe3881786418d9365bc27b992ca1fd6ccc6e
Abulmajeed <alrashabdulmajeed@gmail.com>
2026-08-26 00:25:27 +0300

    Publish encrypted ITIL coach

---
+++ README.md
@@ -0,0 +1,3 @@
+# Encrypted ITIL Coach hosting
+
+The application payload is AES-256-GCM encrypted. No passphrase, course content, answers, or learner progress is stored in this repository.
---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="itil-private-1787693090787";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
